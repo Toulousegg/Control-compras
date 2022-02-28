60c81d78c8daa93fcef961787c29ae53fcd84110
Toulousegg <franklin.m.mogollon@gmail.com>
2022-02-28 20:57:41 -0300

    programa funcional solo con int

--- app.py
+++ app.py
@@ -12,12 +12,13 @@ if a == 'si':
     for answer in b:
         i+=1
         productos = input('Dime el producto: ')
-        precio = float(input('Dime el precio del producto: ')) #error a la hora de transforma a float, no se que ocurre...
+        precio = int(input('Dime el precio del producto: ')) #error a la hora de transforma a float, no se que ocurre...
         lista_de_precios = [precio]#esto deberia guardar todos los valores puestos en precio para después sumarlos, aún no se como hacerlo
-        txt.write(f'{productos}...............') #no obtiene más de un argumento y yo necesito escribir 3 en el .txt, tratare de cambiar de metodo, SOLUCIONADO(?        txt.write(f'{precio}...............')
-        txt.write(f'Articulo número {i}\n')
+        txt.write(f'{productos}...............')
+        txt.write(f'{precio}...............')
+        txt.write(f'Articulo numero {i}\n')
 
-        if productos == 'Termine' 'termine':
+        if productos == 'termine':
             txt.close()
             #suma de todos los valores escritos en precio para dar un total y printiarlo
             break
