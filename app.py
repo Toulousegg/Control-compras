08a009dced0ff6511beb4fa6c966ee01c311be95
John Doe <franklin.m.mogollon@gmail.com>
2022-06-19 19:06:38 -0300

    funcional

--- app.py
+++ app.py
@@ -26,18 +26,23 @@ if a == 'si':
         
         precios = float(input('Dime el precio del producto: '))
         
-        if caracter_separador in precios:
-            separado = precios.split(caracter_separador)
-            n1 = lambda separado, x: [separado[i:i+x] for i in range(0, len(separado), x)] #separador de elementos dentro de listas
-            output = n1(separado, x) #acá separé los elementos pero aún no están guardados en ninguna variable diferente para poder hacer la multiplicación
-            #tengo que guardar los dos números en variables separadas para multiplicarlas
-            lista_de_precios.append(output)
-            continue
+        cantidad = int(input('¿Cuantos articulos vas a llevar?:'))
+
+        #if cantidad == int:
+        cantidad_total = cantidad*precios
+
+        #if caracter_separador in precios:
+            #separado = precios.split(caracter_separador)
+            #n1 = lambda separado, x: [separado[i:i+x] for i in range(0, len(separado), x)] #separador de elementos dentro de listas
+            #output = n1(separado, x) #acá separé los elementos pero aún no están guardados en ninguna variable diferente para poder hacer la multiplicación
+            ##tengo que guardar los dos números en variables separadas para multiplicarlas
+            #lista_de_precios.append(output)
+            #continue
             #la idea es separar los dos números y ponerlos en variables diferentes para después multiplicarlos y poner el total de la multiplicación en la lista de suma
 
-        lista_de_precios.append(precios) #esto deberia guardar todos los valores puestos en precio para después sumarlos, aún no se como hacerlo
+        lista_de_precios.append(cantidad_total)
         txt.write(f'{productos}               ')
-        txt.write(f'{precios}$               ')
+        txt.write(f'{cantidad_total}$               ')
         txt.write(f'Articulo numero {i}\n')
 
 if a == 'no':
